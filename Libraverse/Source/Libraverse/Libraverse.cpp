7cc6a0fd0251f6eab3f505a92061561bfeb626bc
Wingseter <wingseter@gmail.com>
2022-05-23 01:35:52 +0900

    Session Create, Connect, MainMenu page with Error Handling, Chaacter Create

--- Libraverse/Source/Libraverse/Libraverse.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Libraverse.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Libraverse, "Libraverse" );
