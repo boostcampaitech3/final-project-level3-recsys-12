3c60575fc5daf4b89453d744299c7a451aba6173
jonghyun_choi <jonghyun2016@naver.com>
2022-05-31 11:27:26 +0900

    Add Item Crud

--- Backend/db/crud.py
+++ Backend/db/crud.py
@@ -30,17 +30,48 @@ def create_user(db: Session, user: schemas.UserCreate, user_info: schemas.User):
     db.refresh(db_user)
 
 
-def get_recent_item(db: Session, skip: int = 0, limit: int = 2):
-    return db.query(models.Book).order_by('publication_year').offset(skip).limit(limit).all()
-
-
-def get_item(db:Session, item_id: str):
-    return db.query(models.Book).filter(models.Book.id == item_id).first()
-
-
 def all_genres(db:Session):
     return db.query(models.Genre).all()
 
 
-def get_item_by_genre(db:Session, genre_id: int):
+# 최신 책 찾기
+def get_recent_item(db: Session, skip: int = 0, limit: int = 2):
+    return db.query(models.Book).order_by(desc(models.Book.publication_year)).offset(skip).limit(limit).all()
+
+# ID로 책 찾기
+def get_item(db:Session, item_id: str):
+    return db.query(models.Book).filter(models.Book.id == item_id).first()
+
+# 제목으로 책 찾기
+def get_items_by_title(db:Session, title: str):
+    return db.query(models.Book).filter(models.Book.title == title).first()
+
+
+# 해당 연도 책 찾기
+def get_items_on_year(db:Session, year: int):
+    return db.query(models.Book).filter(models.Book.publication_year == year).first()
+
+
+# 각 장르별 책 정보 모음
+def get_items_by_genre(db:Session, genre_id: int):
     return db.query(models.Genre).filter(models.Genre.id == genre_id).first().books
+
+# 각 저자별 책 정보 모음
+def get_items_by_author(db:Session, author_id: int):
+    return db.query(models.Author).filter(models.Author.id == author_id).first().books
+
+
+# 책 제목으로 검색하기
+def search_by_title(db:Session, search_text: str):
+    search = "%{}%".format(search_text)
+    return db.query(models.Book).filter(models.Book.title.like(search)).all()
+
+# 책 내용으로 검색하기
+def search_by_description(db:Session, search_text: str):
+    search = "%{}%".format(search_text)
+    return db.query(models.Book).filter(models.Book.description.like(search)).all()
+
+# 저자로 검색하기
+def search_by_author(db:Session, search_text: str):
+    search = "%{}%".format(search_text)
+    return db.query(models.Author).filter(models.Author.name.like(search)).all()
