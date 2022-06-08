ebeac4c5985c69affacdf282a85f54c7ef02706c
c0natus <hks8946@gmail.com>
2022-06-08 09:32:18 +0900

    web_ver_1

--- Backend/__main__.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-    # nohup /opt/conda/envs/web/bin/python -u /opt/ml/recsys12/Backend/__main__.py > /opt/ml/web_test.log &
-    uvicorn.run("main:app", host="0.0.0.0", port=30002, reload=True)
